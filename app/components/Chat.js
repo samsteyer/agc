8b3a69fd7a9b8ecf1c9d3a8ad562e44dc36f605b
Sam Steyer <sam.steyer@gmail.com>
2023-12-18 14:37:55 -0800

    Add a submit button

--- app/components/Chat.js
+++ app/components/Chat.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import styles from './Chat.module.css';
 import TextField from '@mui/material/TextField';
+import CardActions from '@mui/material/CardActions';
+import Button from '@mui/material/Button';
 
 export default function Chat({handleSubmit, questionInput, setQuestionInput, lastQuestion, result}) {
   return (
@@ -16,6 +18,9 @@ export default function Chat({handleSubmit, questionInput, setQuestionInput, las
           value={questionInput}
           onChange={(e) => setQuestionInput(e.target.value)}
         />
+        <CardActions className={styles.submitButton}>
+          <Button size="small" type="submit">Ask</Button>
+        </CardActions>
       </form>
       {lastQuestion && <div className={styles.lastQuestion}>{lastQuestion}</div>}
       {result && <div className={styles.result}>{result}</div>}
