43c27a5a592b86d8cddf22f976da89cd6c5afbfe
Sam Steyer <sam.steyer@gmail.com>
2023-12-18 15:13:31 -0800

    Re-adding components

---
+++ app/components/Navbar.js
@@ -0,0 +1,23 @@
+import styles from './NavBar.module.css'
+
+import Box from '@mui/material/Box';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+export default function Navbar() {
+  return (
+    <Box className={styles.navBox}>
+      <AppBar position="static">
+        <Toolbar className={styles.topNav}>
+          <Typography variant="h6" component="div" className={styles.navItem}>
+            Chat
+          </Typography>
+          <Typography variant="h6" component="div" className={styles.navItem}>
+            Login
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
